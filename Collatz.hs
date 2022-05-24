24ca4295357efdd7b8737ee6b1f25f3d58507a98
Davidrk31 <89051979+Davidrk31@users.noreply.github.com>
2022-05-24 02:01:18 -0500

    Update Collatz.hs

--- Collatz.hs
+++ Collatz.hs
@@ -1,1 +1,9 @@
-
+collatz :: Int -> [Int]
+collatz 1 = [1]
+collatz n
+    | even n =  n : collatz (n `div` 2)
+    | odd n  =  n : collatz (n * 3 + 1)
+ 
+main = do
+  putStrLn "La sucesion de 14 es:"
+  print(collatz 14)
