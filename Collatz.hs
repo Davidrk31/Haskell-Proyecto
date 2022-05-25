b6c30880975090cb813ac04adf931a1483535e80
Davidrk31 <89051979+Davidrk31@users.noreply.github.com>
2022-05-24 20:21:08 -0500

    Update Collatz.hs

--- Collatz.hs
+++ Collatz.hs
@@ -1,10 +1,12 @@
 collatz :: Integer -> [Integer]
+collatz 0 = [0]
 collatz 1 = [1]
 collatz n
     | even n =  n : collatz (n `div` 2)
     | odd n  =  n : collatz (n * 3 + 1)
  
 main = do
+  putStrLn "Escriba un numero entero positivo"
   numero <- getLine 
   let n = read numero :: Integer
   print("La sucesion de " ++ numero ++ " es :")
